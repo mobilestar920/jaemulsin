be38620508e15c3a146b1211915b92570809542d
root <root@WZYX-1.localdomain>
2021-04-04 14:33:43 +0000

    Fix issue

--- public/js/app.js
+++ public/js/app.js
@@ -49889,9 +49889,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/Jaemulsin/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /Applications/MAMP/htdocs/Jaemulsin/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Jaemulsin/resources/sass/index.scss */"./resources/sass/index.scss");
+__webpack_require__(/*! /var/www/html/jaemulsin/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /var/www/html/jaemulsin/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /var/www/html/jaemulsin/resources/sass/index.scss */"./resources/sass/index.scss");
 
 
 /***/ })
