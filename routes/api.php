32f7e877c595d5924b9000d47b6517627f345749
mobile star <mobilestar920@gmail.com>
2021-04-04 21:19:54 -0700

    - Fix issue

--- routes/api.php
+++ routes/api.php
@@ -13,26 +13,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix' => 'v1/auth'], function($router) {
-    Route::post('register/customer', 'API\\V1\\Controllers\\CustomerLoginController@register');
-    Route::post('login/customer', 'API\\V1\\Controllers\\CustomerLoginController@login');
+    Route::post('register/customer', 'Api\\V1\\Controllers\\CustomerLoginController@register');
+    Route::post('login/customer', 'Api\\V1\\Controllers\\CustomerLoginController@login');
 });
 
 Route::group(['middleware' => ['assign.guard:api', 'jwt.auth'], 'prefix' => 'v1'], function($router) {
-    Route::get('apps', 'API\\V1\\Controllers\\AppListController@index');
-    Route::get('apps/ids', 'API\\V1\\Controllers\\AppListController@downloadableAppIds');
-    Route::get('me/{id}', 'API\\V1\\Controllers\\AppListController@caishenDownload');
-    Route::get('news/latest', 'API\\V1\\Controllers\\AppListController@getLatestNews');
-    Route::post('me/login/status', 'API\\V1\\Controllers\\AppListController@userAvailable');
+    Route::get('apps', 'Api\\V1\\Controllers\\AppListController@index');
+    Route::get('apps/ids', 'Api\\V1\\Controllers\\AppListController@downloadableAppIds');
+    Route::get('me/{id}', 'Api\\V1\\Controllers\\AppListController@caishenDownload');
+    Route::get('news/latest', 'Api\\V1\\Controllers\\AppListController@getLatestNews');
+    Route::post('me/login/status', 'Api\\V1\\Controllers\\AppListController@userAvailable');
 });
 
 
 Route::group(['middleware' => ['assign.guard:api', 'jwt.auth'], 'prefix' => 'app'], function($router) {
-    Route::get('download/{id}', 'API\\V1\\Controllers\\AppListController@download');
-    Route::get('resource/{id}', 'API\\V1\\Controllers\\AppListController@resourceDownload');
-    Route::get('mile/{id}', 'API\\V1\\Controllers\\AppListController@getMileResource');
-    Route::get('changbao/{id}', 'API\\V1\\Controllers\\AppListController@changbaoDownload');
+    Route::get('download/{id}', 'Api\\V1\\Controllers\\AppListController@download');
+    Route::get('resource/{id}', 'Api\\V1\\Controllers\\AppListController@resourceDownload');
+    Route::get('mile/{id}', 'Api\\V1\\Controllers\\AppListController@getMileResource');
+    Route::get('changbao/{id}', 'Api\\V1\\Controllers\\AppListController@changbaoDownload');
 });
 
 Route::group(['prefix' => 'download'], function($router) {
-    Route::get('caishen/latest', 'API\\V1\\Controllers\\AppListController@caishenFreeDownload');
+    Route::get('caishen/latest', 'Api\\V1\\Controllers\\AppListController@caishenFreeDownload');
 });
